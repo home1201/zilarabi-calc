5dc468b5e4c6060fefa899f69aac541d8be9fa68
homeqwert <home1201@gmail.com>
2024-09-22 13:50:41 +0900

    베이스 git 생성

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require("autoprefixer"),
+    require("postcss-nested-ancestors"),
+    require("postcss-nested"),
+    require("postcss-custom-media"),
+  ],
+};

13e58c9c221f0be12af28eb1c4ddfa1793be0d70
데스크톱 <homeqwert@homeqwertpc.>
2024-10-20 20:56:47 +0900

    sass embedded update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {

ad6712d3fc08e48f6fb3d4e55c41f58dc31228c7
homeqwert <home1201@gmail.com>
2024-12-10 17:59:04 +0900

    초기화 버튼 추가 중(애니메이션, 기능 추가 필요)

--- src/components/TheCost.vue
+++ src/components/TheCost.vue
@@ -5,13 +5,31 @@
     </div>
     <div class="received">
       <label class="received__label">
-        손님이 준 돈 : <input v-model="received" type="number" class="received__input">
+        손님이 준 돈 : <input
+          v-model="received"
+          type="number"
+          class="received__input"
+        >
       </label>
       원
     </div>
     <div class="change">
       거스름돈 :
       <span class="change__value">{{ changeMessage }}</span>
+    </div>
+    <button
+      type="button"
+      class="change__reset"
+      popovertarget="notify-reset"
+    >
+      모두 지우기
+    </button>
+    <div
+      id="notify-reset"
+      class="dummy"
+      popover
+    >
+      지우기 완료!
     </div>
   </div>
 </template>
@@ -37,6 +55,9 @@ const changeMessage = computed(() => {
 .calc {
   position: sticky;
   bottom: 0;
+  display: grid;
+  grid-template: 1fr 1fr 1fr / 1fr 1fr;
+  grid-auto-flow: column;
   width: 100%;
   padding-block-end: var(--space-300);
   padding-inline-start: var(--space-300);
@@ -52,6 +73,16 @@ const changeMessage = computed(() => {
   &__value {
     color: var(--color-secondary-500);
   }
+
+  &__reset {
+    grid-row: span 3;
+    margin: var(--space-500) var(--space-600);
+    font-weight: 600;
+    background-color: var(--color-secondary-300);
+    filter: drop-shadow(0 1px 1px var(--color-secondary-500));
+    border: none;
+    border-radius: var(--radius-min);
+  }
 }
 
 .received {
@@ -66,6 +97,28 @@ const changeMessage = computed(() => {
   &__input {
     width: 4rem;
     margin-inline-start: var(--space-200);
+  }
+}
+
+#notify-reset {
+  &:popover-open {
+    top:50%;
+    left: 50%;
+    display: block;
+    padding: var(--space-300) var(--space-400);
+    background-color: var(--color-primary-100);
+    filter: drop-shadow(0 1px 1px var(--color-primary-400));
+    border: none;
+    border-radius: var(--radius-min);
+    transition-duration: 1s;
+    transition-property: opacity, display;
+    translate: -50% -50%;
+    transition-behavior: allow-discrete;
+  }
+
+  &.close {
+    display: none;
+    opacity: 0;
   }
 }
 </style>
